0c82812000e58ba0e732735ba8de64bc3bf77849
Francis Ouellet <francis.ouellet@optelgroup.com>
2019-04-16 14:05:06 -0400

    update to pre-release 3, bigger machine

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ cd /home/minecraft
 sudo su
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/eula.txt eula.txt
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/server.properties server.properties
-curl https://launcher.mojang.com/v1/objects/6f27430bcd9b06d3dcb5d2966c75d5e491915c9c/server.jar > server.jar
+curl https://launcher.mojang.com/v1/objects/6b747e1338e1aa058146032a659cf654c446552d/server.jar > server.jar
 java -Xms1G -Xmx3G -d64 -jar server.jar nogui
