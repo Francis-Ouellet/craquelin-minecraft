aa4d8d4af9a018f2ff26c512dc2e8dd673a4c4ae
Francis-Ouellet <franc.ouelle93@gmail.com>
2019-04-18 09:35:19 -0400

    Update to pre-release 5

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ cd /home/minecraft
 sudo su
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/eula.txt eula.txt
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/server.properties server.properties
-curl https://launcher.mojang.com/v1/objects/cf967a23b452ab474bf7bcb69fd029a5f8b84bba/server.jar > server.jar
+curl https://launcher.mojang.com/v1/objects/5d550762b9c82ab4fe9f259c14fcf7bf7ed8017a/server.jar > server.jar
 java -Xms1G -Xmx3G -d64 -jar server.jar nogui
