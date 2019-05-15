a83287a70294f7cfb64617f7fb920cc0d28cff76
Francis Ouellet <francis.ouellet@optelgroup.com>
2019-05-14 20:31:29 -0400

    Updated to 1.14.1, added ops, added datapacks, updated build / start scripts

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ sudo apt-get install -y default-jre-headless
 sudo mkdir -p /home/minecraft
 cd /home/minecraft
 sudo su
-gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/eula.txt eula.txt
-gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/server.properties server.properties
-curl https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar > server.jar
+gsutil -r cp gs://craquelin-web.appspot.com/minecraftconfigs/ .
+curl https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar > server.jar
 java -Xms1G -Xmx3G -d64 -jar server.jar nogui
