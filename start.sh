b323153e87e8cccf336e50c6d1f207956e146c43
Francis Ouellet <francis.ouellet@optelgroup.com>
2019-05-14 21:26:30 -0400

    added multithreading to datapacks copy

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ sudo su
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/eula.txt eula.txt
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/server.properties server.properties
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/ops.json ops.json
-gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/world/datapacks/*.zip world/datapacks/
+gsutil -m cp gs://craquelin-web.appspot.com/minecraftconfigs/world/datapacks/*.zip world/datapacks/
 curl https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar > server.jar
 java -Xms1G -Xmx3G -d64 -jar server.jar nogui
