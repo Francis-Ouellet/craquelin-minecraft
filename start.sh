8be34ce5462c15bee6323de418429af6b2df4f10
Francis Ouellet <francis.ouellet@optelgroup.com>
2019-05-14 20:53:04 -0400

    copying minecraft configs content only

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ sudo apt-get install -y default-jre-headless
 sudo mkdir -p /home/minecraft
 cd /home/minecraft
 sudo su
-gsutil cp -r gs://craquelin-web.appspot.com/minecraftconfigs/ .
+gsutil -m cp -r gs://craquelin-web.appspot.com/minecraftconfigs/** .
 curl https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar > server.jar
 java -Xms1G -Xmx3G -d64 -jar server.jar nogui
