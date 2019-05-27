455c5132f8f431fb532c7ebba466c0562bd54ae0
Francis-Ouellet <franc.ouelle93@gmail.com>
2019-05-27 11:10:29 -0400

    Updated to 1.14.2

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/eula.txt eula.txt
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/server.properties server.properties
 gsutil cp gs://craquelin-web.appspot.com/minecraftconfigs/ops.json ops.json
 gsutil -m cp gs://craquelin-web.appspot.com/minecraftconfigs/world/datapacks/*.zip world/datapacks/
-curl https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar > server.jar
+curl https://launcher.mojang.com/v1/objects/808be3869e2ca6b62378f9f4b33c946621620019/server.jar > server.jar
 java -Xms1G -Xmx3G -d64 -jar server.jar nogui
